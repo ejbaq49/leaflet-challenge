673db273d96638170181f27fcf991c0c62932592
EJBaquet <ejbaquet@yahoo.com>
2020-09-16 21:44:14 -0400

    add earthquake url

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,11 +1,10 @@
-// Creating map object centered on Las Vegas :)
-var myMap = L.map("map", {
-  center: [36, -115],
-  zoom: 6,
-});
+// Link to earthquake data
+var queryUrl =
+  "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
 // Adding tile layer to the map
-L.tileLayer(
+var streetmap = L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
@@ -16,4 +15,11 @@ L.tileLayer(
     id: "mapbox/streets-v11",
     accessToken: API_KEY,
   }
-).addTo(myMap);
+);
+
+// Creating map object centered on Salt Lake City
+var myMap = L.map("map", {
+  center: [40.76, -111.89],
+  zoom: 5,
+  layers: [streetmap],
+});
